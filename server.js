e03740123c790aa52ce38a915e214fd93db42a0d
VictoriaArda <ardavictoria@gmail.com>
2020-07-13 22:35:02 -0400

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 8484;
+const PORT = 3000;
 
 const app = express();
 
